b5ccef49df45b745dfd9fc1ca985eff48a0bb515
Mohammad <mohammadgithub2024@gmail.com>
2025-03-20 14:05:11 +0330

    add features : categories model

--- src/models/products.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = mongoose.Schema({
-
-})
-
-const model = mongoose.model("Product" , schema);
-
-module.exports = model
---
+++ src/utils/uploader.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const multer = require("multer");
+const crypto = require("crypto");
+
+module.exports = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "..", "public", "covers"));
+  },
+  filename: (req, file, cb) => {
+    const fileName = Date.now() + "-" + Math.floor(Math.random() * 9999);
+
+    // const hashedFilename = crypto
+    //   .createHash("SHA256")
+    //   .update(file.originalname)
+    //   .digest("hex");
+
+    const ext = path.extname(file.originalname);
+    cb(null, fileName + ext);
+  },
+});
